9afabd863c6b57d8f2866a657c50c5d346a6c9bd
jixer <me@bloggedbychris.com>
2013-02-03 14:29:13 -0800

    Cleaned up files and server.js

--- server.js
+++ server.js
@@ -10,18 +10,19 @@ var express = require('express')
 
 var app = module.exports = express();
 
-app.configure(function() {
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use("/js", express.static(__dirname + '/js'));
-  app.use("/css", express.static(__dirname + '/css'));
-  app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use("/js", express.static(path.join(__dirname, '/SRC/js')));
+    app.use("/css", express.static(path.join(__dirname, '/SRC/css')));
+    app.use('/img', express.static(path.join(__dirname, '/SRC/img')));
+    app.use('/public', express.static(path.join(__dirname, '/public')));
 });
 
 app.configure('development', function(){
@@ -37,14 +38,11 @@ app.patch('/apis/', apiroutes.update);
 // MVC app routes
 app.get("/:uuid", routes.redirect);
 
-/*
-app.post('/', routes.create);
-app.get('/', routes.request);
-app.get('/search', routes.getsearch);
-app.get('/delete/:uuid', routes.delete);
-app.post('/search', routes.postsearch);
-app.get('/:uuid', routes.find);
-*/
+// override default
+app.get('/', function (req, res) {
+    res.redirect('/public/main.html');
+});
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
