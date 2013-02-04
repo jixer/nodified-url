f161ae3a03e627c718cec09959000663aae186cf
jixer <me@bloggedbychris.com>
2013-02-03 19:38:54 -0800

    Cleanup

--- SRC/js/delete.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$("#delete").click(function() {
-		var tr = $(this).parent().parent();
-		$.getJSON($(this).attr("href"), function() {
-			$(tr).addClass("deleted");
-		});
-	});
-});
